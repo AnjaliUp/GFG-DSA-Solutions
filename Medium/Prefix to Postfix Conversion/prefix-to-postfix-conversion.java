00947d1724af93cf118dfac2cde5c354a8f36260
Anjali Upadhyay <125463983+AnjaliUp@users.noreply.github.com>
2024-05-26 11:41:52 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Prefix to Postfix Conversion/prefix-to-postfix-conversion.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            sc.nextLine();
+            String s = sc.next();
+            Solution obj = new Solution();
+            String ans = obj.preToPost(s);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static String preToPost(String pre) {
+        // code here
+        Stack<String> s = new Stack<>();
+        for(int i = pre.length()-1; i>=0; i--){
+            char ch = pre.charAt(i);
+            if(Character.isLetter(ch)){
+                s.push(String.valueOf(ch));
+            }
+            else{
+                String a = s.pop();
+                String b = s.pop();
+                s.push(a+b+ch);
+            }
+        }
+        
+        return s.pop();
+    }
+}
