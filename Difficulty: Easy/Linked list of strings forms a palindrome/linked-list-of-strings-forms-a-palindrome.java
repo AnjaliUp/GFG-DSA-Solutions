aa33ec93fafd87b63c77911063342c9e5c3bd493
Anjali Upadhyay <125463983+AnjaliUp@users.noreply.github.com>
2024-07-02 18:45:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Linked list of strings forms a palindrome/linked-list-of-strings-forms-a-palindrome.java
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    String data;
+    Node next;
+
+    Node(String x) {
+        data = x;
+        next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    LinkedList() { head = null; }
+
+    void addNode(String str) {
+        if (head == null) {
+            head = new Node(str);
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = new Node(str);
+        }
+    }
+
+    void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int K = sc.nextInt();
+            LinkedList list = new LinkedList();
+
+            for (int i = 0; i < K; i++) {
+                String str = sc.next();
+                list.addNode(str);
+            }
+
+            Solution ob = new Solution();
+            boolean ans = ob.compute(list.head);
+            if (ans) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+        }
+        sc.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public boolean compute(Node root) {
+        String s = "";
+        
+        //add data of each node of LL to String s
+        while(root != null)
+        {
+            s+=root.data;
+            root = root.next;
+        }
+        
+        //iterate through string in normal and reversed order and compare
+        
+        for(int i = s.length() - 1, j=0; i >= 0; i--,j++)
+        {
+            if(s.charAt(i) != s.charAt(j))
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
