fb5120656b7ad04fe7648a776e16e5eb7e0f145d
Anjali Upadhyay <125463983+AnjaliUp@users.noreply.github.com>
2024-09-16 19:21:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Leaders/array-leaders.java
+++ Difficulty: Easy/Array Leaders/array-leaders.java
@@ -42,35 +42,28 @@ class Array {
 
 // } Driver Code Ends
 
-
 class Solution {
     // Function to find the leaders in the array.
     static ArrayList<Integer> leaders(int n, int arr[]) {
         // Your code here
-        ArrayList<Integer> ans= new ArrayList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
   
-  for (int i = 0; i < n; i++) {
-    boolean leader = true;
+        // Last element of an array is always a leader,
+        // push into ans array.
+        int max = arr[n - 1];
+        ans.add(max);
+    
+        // Start checking from the end whether a number is greater
+        // than the max number from right, hence leader.
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] >= max) {  // Use >= to include equal elements as leaders
+                ans.add(arr[i]);
+                max = arr[i];
+            }
+        }
 
-    //Checking whether arr[i] is greater than all 
-    //the elements in its right side
-    for (int j = i + 1; j < n; j++)
-      if (arr[j] > arr[i]) {
-          
-        // If any element found is greater than current leader
-        // curr element is not the leader.
-        leader = false;
-        break;
-      }
-
-    // Push all the leaders in ans array.
-    if (leader)
-    ans.add(arr[i]);
-
-  }
-  
-  return ans;
-   
-
+        // Reverse the list to maintain the order as in the original array
+        Collections.reverse(ans);
+        return ans;
     }
 }
