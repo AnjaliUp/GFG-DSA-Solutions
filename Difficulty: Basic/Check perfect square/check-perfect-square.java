732cdf8796f6a89918a731cd276476bc503a6653
Anjali Upadhyay <125463983+AnjaliUp@users.noreply.github.com>
2024-09-25 19:53:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check perfect square/check-perfect-square.java
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.checkPerfectSquare(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static int checkPerfectSquare(int N){
+        // code here
+        for(int i= 1; i*i<=N; i++){
+            if(i*i==N) return 1;
+        }
+        
+        return 0;
+    }
+}
